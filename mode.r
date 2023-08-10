8ed8db38d8bed6f6c0eaaf530f91fc7c2cb68a64
yaswanth <113416864+kingyashu@users.noreply.github.com>
2023-08-10 12:21:51 +0530

    Create mode.r

---
+++ mode.r
@@ -0,0 +1,10 @@
+getmode<-function(v){
+  uniqv<-unique(v)
+  uniqv[which.max(tabulate(match(v,uniqv)))]
+}
+v<-c(2,1,2,3,1,2,3,4,1,5,55,3,2,3)
+result<-getmode(v)
+print(result)
+charv<-c("0","it","the","it","it")
+result<-getmode(charv)
+print(result)
